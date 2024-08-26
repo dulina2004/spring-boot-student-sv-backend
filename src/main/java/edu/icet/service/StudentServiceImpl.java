212c84d9d5908475ffb3fa2fa14ce6cd56509ed4
dulina2004 <dulinagunasinghe.com>
2024-08-26 20:35:53 +0530

    milestone 1

---
+++ src/main/java/edu/icet/service/StudentServiceImpl.java
@@ -0,0 +1,41 @@
+package edu.icet.service;
+
+import edu.icet.dto.Student;
+import edu.icet.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    StudentRepository repository;
+
+    @Override
+    public List<Student> getStudent() {
+        List<Student> all = repository.findAll();
+        return all;
+    }
+
+    @Override
+    public void addStudent(Student student) {
+        repository.save(student);
+    }
+
+    @Override
+    public void deleteStudentById(Integer id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+        repository.save(student);
+    }
+
+    @Override
+    public List<Student> findByName(String name) {
+        return repository.findByName(name);
+    }
+
+}
